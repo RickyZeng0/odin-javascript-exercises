220c1a1764e2c421632ea06042193a792914d60a
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-05 11:02:08 +0800

    finish exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let celsius = (x-32)* 5/9;
+  let roundedCelsius = Math.round(celsius*10)/10;
+  return roundedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let fahrenheit = x * 9/5 + 32;
+  let roundedCelsius = Math.round(fahrenheit*10)/10;
+  return roundedCelsius;
 };
 
 // Do not edit below this line
