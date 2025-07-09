e421e751c93bd5e5b809ff85fbdf9f3c68f7d749
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-09 15:46:00 +0800

    finish exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort( (a,b) => {
+        if(b.yearOfDeath === undefined) b.yearOfDeath = 2025;
+        if(a.yearOfDeath === undefined) a.yearOfDeath = 2025;
+        let aAge = a.yearOfDeath - a.yearOfBirth;
+        let bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge ;
+    })[0];
 };
 
 // Do not edit below this line
