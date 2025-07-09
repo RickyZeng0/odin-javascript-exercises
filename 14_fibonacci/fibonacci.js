5de4f71ebe1b292a38f4eff29947b15bef432440
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-09 15:16:34 +0800

    finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function f(n) {
+    if(n < 0) return "OOPS"; 
+    if(n == 0) return 0;
+    if(n == 1 || n == 2) return 1;
+    else return f(n-1) + f(n-2);
 };
 
 // Do not edit below this line
