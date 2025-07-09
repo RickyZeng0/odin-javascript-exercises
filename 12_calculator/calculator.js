f3bb0454953d608e7283dcc49ce212de5d8e00ab
Ricky_Zeng <zengchiwai08@gmail.com>
2025-07-09 14:03:23 +0800

    finish exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce( ( (sum,element) => sum += element),0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( ( (multiplies,element) => multiplies *= element),1);
 };
 
-const power = function() {
-	
+const power = function(number,index) {
+	return number ** index;
 };
 
-const factorial = function() {
-	
+const factorial = function f(n) {
+	if(n==1 || n == 0) return 1;
+  else return n * f(n-1);
 };
 
 // Do not edit below this line
